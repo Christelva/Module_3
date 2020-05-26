eaadeea79e50083364ee839eafa4d103756ea9f0
Christelva <cvanaanholt@hotmail.com>
2020-05-26 15:52:01 +0200

    Add files via upload

---
+++ Opdracht_003_M3/script.js
@@ -0,0 +1,24 @@
+let age = 10;
+let isFemale = false;
+let driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    console.log("U mag rijden")
+}
+else {
+    console.log("U mag niet rijden")
+}
+
+if (isFemale == true){
+    console.log("U bent een vrouw")
+}
+else {
+    console.log("u bent geen vrouw")
+}
+
+if (age >= 18) {
+    console.log("Je mag naar binnen")
+}
+else {
+    console.log("Je bent te jong, je mag niet naar binnen.")
+}
