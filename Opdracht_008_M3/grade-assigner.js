846bda30648df9fa6a965b276647ee59c7caeb12
Christelva <cvanaanholt@hotmail.com>
2020-05-29 20:31:50 +0200

    Add files via upload

---
+++ Opdracht_008_M3/grade-assigner.js
@@ -0,0 +1,21 @@
+let grade="";
+
+const assignGrade = function(i){
+    if (i > 90) {
+        return grade='A';
+    } else if (i > 80) {
+        return grade='B';
+    } else if (i > 70) {
+        return grade='C';
+    } else if (i > 65) {
+        return grade='D';
+    } else {
+        return grade='F';
+    }
+}
+
+for(i=60; i<=100; i++){
+assignGrade(i);
+console.log (i);
+console.log("Voor "+i+" punten krijg je een "+grade);
+}
